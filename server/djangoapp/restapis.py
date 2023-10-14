a86135210f223ccd49ce63fad737b2f8c92d516b
Manuel Pessoa <faimpessoa@gmail.com>
2023-10-14 04:56:07 -0400

    NLU Done
    Signed-off-by: Manuel Pessoa <faimpessoa@gmail.com>

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -12,17 +12,30 @@ from requests.auth import HTTPBasicAuth
 def get_request(url, **kwargs):
     print(kwargs)
     print("GET from {} ".format(url))
-    try:
-        # Call get method of requests library with URL and parameters
-        response = requests.get(url, headers={'Content-Type': 'application/json'},
-                                    params=kwargs)
-    except:
-        # If any error occurs
-        print("Network exception occurred")
+    api_key = ""
+    if "apikey" in kwargs:
+        api_key = kwargs["apikey"]
+        try:
+            print(f"With key:{api_key}")
+            # Call get method of requests library with URL and parameters
+            response = requests.get(url, params=kwargs, headers={'Content-Type': 'application/json'},
+                                        auth=HTTPBasicAuth('apikey', api_key))
+        except:
+            # If any error occurs
+            print("Network exception occurred")
+    else:
+        try:
+            # Call get method of requests library with URL and parameters
+            response = requests.get(url, headers={'Content-Type': 'application/json'},
+                                        params=kwargs)
+        except:
+            # If any error occurs
+            print("Network exception occurred")
+
     status_code = response.status_code
     print("With status {} ".format(status_code))
     json_data = json.loads(response.text)
-    #print(json_data)
+    print(f"With jsonData: {json_data}")
     return json_data
 
 # Create a `post_request` to make HTTP POST requests
@@ -67,6 +80,7 @@ def get_dealer_reviews_from_cf(url, **kwargs):
         for dealer_review in json_result:
             # Get its content in `doc` object
             review_doc = dealer_review
+            sent = analyze_review_sentiments(review_doc["review"])
             # Create a CarDealer object with values in `doc` object
             review_obj = DealerReview(dealership=review_doc["dealership"],
                 name=review_doc["name"],
@@ -76,7 +90,7 @@ def get_dealer_reviews_from_cf(url, **kwargs):
                 car_make=review_doc["car_make"],
                 car_model=review_doc["car_model"],
                 car_year=review_doc["car_year"],
-                sentiment="None",
+                sentiment=sent,
                 id=review_doc["id"])
             
             results.append(review_obj)
@@ -85,9 +99,20 @@ def get_dealer_reviews_from_cf(url, **kwargs):
 
 
 # Create an `analyze_review_sentiments` method to call Watson NLU and analyze text
-# def analyze_review_sentiments(text):
+def analyze_review_sentiments(text):
 # - Call get_request() with specified arguments
 # - Get the returned sentiment label such as Positive or Negative
-
+    url = "https://api.au-syd.natural-language-understanding.watson.cloud.ibm.com/instances/96afe15a-80ff-48b2-95b5-e904a60a7601/v1/analyze"
+    #apikey = "UWmQfDNS8ciMNOavBOXgv4zzJEaaW7sr6XgpBrkkBjzf"
+    #version = "2019-04-07"
+    json_result = get_request(url, version = "2019-04-07", text=text, features="sentiment",
+                       apikey = "UWmQfDNS8ciMNOavBOXgv4zzJEaaW7sr6XgpBrkkBjzf")
+    ret = "No Result"
+    if json_result and "sentiment" in json_result \
+       and "document" in json_result["sentiment"] \
+       and "label" in json_result["sentiment"]["document"]:
+        ret = json_result["sentiment"]["document"]["label"]
+    
+    return ret
 
 
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -94,7 +94,7 @@ def registration_request(request):
 #        return render(request, 'djangoapp/index.html', context)
 def get_dealerships(request):
     if request.method == "GET":
-        url = "https://faimpessoa-3000.theiadocker-3-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/dealerships/get"
+        url = "https://faimpessoa-3000.theiadocker-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/dealerships/get"
         # Get dealers from the URL
         dealerships = get_dealers_from_cf(url)
         # Concat all dealer's short name
@@ -108,7 +108,7 @@ def get_dealerships(request):
 # Create a `get_dealer_details` view to render the reviews of a dealer
 def get_dealer_details(request, dealer_id):
     if request.method == "GET":
-        url = "https://faimpessoa-5000.theiadocker-3-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/api/get_reviews"
+        url = "https://faimpessoa-5000.theiadocker-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/api/get_reviews"
         # Get dealers from the URL
         dealership_reviews = get_dealer_reviews_from_cf(url, id=dealer_id)
         # Concat all dealer's short name
